7424823cd6ca9f245a69404b8233a3978132ad59
hyunjaemoon <calhyunjaemoon@gmail.com>
2024-12-17 21:00:56 -0800

    Include Theme Song to chanceshift, add logging, and remove dual_app

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,53 @@
 import 'package:chanceshfit/menu_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:logging/logging.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key, required this.log});
+
+  final Logger log;
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late final AudioPlayer _player;
+  late final Logger log;
+
+  @override
+  void initState() {
+    super.initState();
+    _player = AudioPlayer();
+    log = widget.log;
+    _init();
+  }
+
+  Future<void> _init() async {
+    try {
+      await _player.setAsset('audio/chanceshift_theme.mp3');
+      await _player.setLoopMode(LoopMode.all);
+    } on PlayerException catch (e) {
+      log.info("Error loading audio source: $e");
+    }
+  }
+
+  @override
+  void dispose() {
+    _player.dispose();
+    super.dispose();
+  }
+
+  void _toggleMusic() {
+    setState(() {
+      if (_player.playing) {
+        _player.pause();
+      } else {
+        _player.play();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +58,16 @@ class MyApp extends StatelessWidget {
         brightness: Brightness.dark,
         primaryColor: Colors.blueGrey,
       ),
-      home: const MenuScreen(),
+      home: Scaffold(
+          appBar: AppBar(
+            actions: [
+              IconButton(
+                  tooltip: 'Toggle music',
+                  onPressed: _toggleMusic,
+                  icon: Icon(_player.playing ? Icons.pause : Icons.play_arrow))
+            ],
+          ),
+          body: const MenuScreen()),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:args/args.dart';
 import 'package:chanceshfit/app.dart';
-import 'package:chanceshfit/dual_app.dart';
 import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
 
 void main(List<String> arguments) {
+  Logger log = Logger('chanceshift');
   var parser = ArgParser()
     ..addFlag('enable-dual-screen', negatable: false, defaultsTo: false);
 
@@ -11,8 +12,8 @@ void main(List<String> arguments) {
   bool enableFeature = results['enable-dual-screen'];
 
   if (enableFeature) {
-    runApp(const MyDualApp());
-  } else {
-    runApp(const MyApp());
+    log.info("Dual screen feature enabled");
   }
+
+  runApp(MyApp(log: log));
 }
